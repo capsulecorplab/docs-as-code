40b8722025153ec0b78b8862a80decd65fef9de3
Sean Marquez <capsulecorplab@gmail.com>
2021-03-07 18:04:37 -0800

    replace Gemfile* w/ docker build script

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-bundle exec asciidoctor-revealjs presentation.adoc
+
+docker run --rm --volume $PWD:/src -w "/src" capsulecorplab/asciidoctor-extended:revealjs 'bundle exec asciidoctor-revealjs -a revealjsdir=https://cdn.jsdelivr.net/npm/reveal.js@3.9.2 presentation.adoc -o public/index.html'
