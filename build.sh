2775e93f247b724415dc6e92ee2f2b3854e3c862
Sean Marquez <capsulecorplab@gmail.com>
2022-07-27 17:16:21 -0600

    update build.sh to check if toolchain is installed
    Check if version 4.1.0 of asciidoctor-revealjs is installed.
    Otherwise, run build via docker.

--- build.sh
+++ build.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
 
-docker run --rm -v "$PWD:/src" -w "/src" asciidoctor/docker-asciidoctor:1.6.0 asciidoctor-revealjs presentation.adoc
+clitool="asciidoctor-revealjs"
+version=4.1.0
+cmd="$clitool presentation.adoc"
+
+if ! $clitool --version | grep $version > /dev/null; then
+    echo "$clitool $version not installed"
+    echo "running build via docker..."
+    docker run --rm -v "$PWD:/src" -w "/src" asciidoctor/docker-asciidoctor:1.6.0 $cmd
+else
+    echo "$clitool $version installed!"
+    echo "running build..."
+    $cmd
+fi
 
 if [ ! -r ./public ]; then
     mkdir public
